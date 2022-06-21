2498334e2cb1a6be358ac559dfa58ad3f24c27e4
Aaron Carlisle <aaroncarlisle.cs@gmail.com>
2022-06-21 16:09:02 -0600

    Begin coding and testing toolbar functions.

--- js/script.js
+++ js/script.js
@@ -1,26 +1,81 @@
 const sketchpad = document.querySelector('.sketchpad');
 const toolbar = document.querySelector('.toolbar');
-let color = 'black';
+
+// Default behavior of the Etch-a-Sketch
+let drawColor = 'black';
+let sketchBackgroundColor = 'white';
 let cells = [];
 for (let i = 0; i < 100; i++) {
     let row = document.createElement('div');
     row.classList.add('row');
-    cells.push(row);
     sketchpad.appendChild(row);
     for (let i = 0; i < 100; i++) {
         let col = document.createElement('div');
         col.classList.add('column');
         col.style.border = '1px solid black';
-        col.addEventListener('mouseover', () => col.style.backgroundColor = color);
         cells.push(col);
         row.appendChild(col);
     }
 }
-function remove() {
-    for (cell of cells) {
-        cell.style.backgroundColor = 'white';
+
+// Toolbar functions
+let pointer = false;
+let pencil = false;
+let pen = false;
+let feather = false;
+let eraser = false;
+
+function removeDrawType() {
+    if (pencil) {
+        for (let cell of cells) {
+            cell.removeEventListener('click', drawlogic);
+        }
+        pencil = false;
+    }
+    else if (pen) {
+        for (let cell of cells) {
+            cell.removeEventListener('mouseover', drawLogic);
+        }
+        pen = false;
     }
 }
-function changeColor(cell) {
-    cell.addEventListener('mouseover', cell.style.backgroundColor = color);
+const drawLogic = function (e) {
+    e.target.style.backgroundColor = drawColor;
 }
+function selectPencil() {
+    if (!pencil) {
+        removeDrawType();
+        for (let cell of cells) {
+            cell.addEventListener('click', drawLogic);
+        }
+        pencil = true;
+    } 
+}
+
+function selectPen() {
+    if (!pen) {
+        removeDrawType();
+        for (let cell of cells) {
+            cell.addEventListener('mouseover', drawLogic);
+        }
+        pen = true;
+    }
+}
+
+
+// toolbar button event listeners
+let penButton = document.querySelector('.pen');
+penButton.addEventListener('click', function () {
+    if (!pen) {
+        removeDrawType();
+        selectPen();
+    }
+});
+
+let pencilButton = document.querySelector('.pencil');
+pencilButton.addEventListener('click', function () {
+    if (!pencil) {
+        removeDrawType();
+        selectPencil()
+    }
+})
